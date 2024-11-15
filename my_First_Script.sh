20a33dd0ae1b72a0fbbd7f2dd7a77c0e1b581400
Ben Redmond <73973708+BenRedmondgithub@users.noreply.github.com>
2024-11-15 12:21:24 +0000

    adding comments
    Added Author, Date & Description

--- my_First_Script.sh
+++ my_First_Script.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
+# Author: Ben Redmond
+# Date Created: 15/11/2024
+# Last Modifed: 15/11/2024
+
+# Descripion 
+# This script prints "this is my first script" to the terminal  
+
 echo "this is my first script"
 exit 0
